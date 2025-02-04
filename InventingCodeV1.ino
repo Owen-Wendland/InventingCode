09ce7fe32c92493e9e504e21955e7860bbeae316
Owen Wendland <90295568+Owen-Wendland@users.noreply.github.com>
2025-02-03 23:56:47 -0800

    Update InventingCodeV1.ino
    Made a rough draft of what the button read will look like

--- InventingCodeV1.ino
+++ InventingCodeV1.ino
@@ -1,9 +1,22 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  for(int pin = 1; pin <= 4; pin++){
+    pinMode(pin, INPUT_PULLUP);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int buttonStates[4] = {0,0,0,0};
+  for(int pin = 1; pin <= 4; pin++){
+    pinMode(pin, INPUT_PULLUP);
+    buttonStates[pin] = digitalRead(pin);
+  }
+  for(int pin = 1; pin <= 4; pin++){
+    if(buttonStates[pin] == HIGH){
+      Serial.println(pin, "High");
+    }
+    else{
+      Serial.println(pin, "Low");
+    }
+  }
 }
