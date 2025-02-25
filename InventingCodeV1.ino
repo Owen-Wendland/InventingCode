bcb6dc3dcbe3b67c5d44ed643d49776906397842
Owen Wendland <90295568+Owen-Wendland@users.noreply.github.com>
2025-02-25 12:04:36 -0800

    Update InventingCodeV1.ino

--- InventingCodeV1.ino
+++ InventingCodeV1.ino
@@ -3,12 +3,12 @@
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-bool but1 = False
-bool but2 = False
-bool but3 = False
-bool but4 = False
+bool but1 = false;
+bool but2 = false;
+bool but3 = false;
+bool but4 = false;
 
-int rerate = 60
+int rerate = 60;
 
 void setup() {
   lcd.begin(16, 2);
@@ -26,37 +26,39 @@ void loop() {
   }
   for(int pin = 1; pin <= 4; pin++){
     if(buttonStates[pin] == HIGH){
-      if pin == 1{
-        but1 = True
+      if(pin == 1){
+        but1 = true;
       }
-      else if pin == 2{
-        but2 = True
+      else if (pin == 2){
+        but2 = true;
       }
-      else if pin == 3{
-        but3 = True
+      else if (pin == 3){
+        but3 = true;
       }
       else{
-        but4 = True
+        but4 = true;
       }
-      Serial.println(pin, "High");
+      //Serial.println(pin, "High");
     }
     else{
-      if pin == 1{
-        but1 = False
+      if (pin == 1){
+        but1 = false;
       }
-      else if pin == 2{
-        but2 = False
+      else if (pin == 2){
+        but2 = false;
       }
-      else if pin == 3{
-        but3 = False
+      else if (pin == 3){
+        but3 = false;
       }
       else{
-        but4 = False
+        but4 = false;
       }
-      Serial.println(pin, "Low");
+      //Serial.println(pin, "Low");
     }
+    Serial.println(String(("1",but1,"2",but2,"3",but3,"4",but4)));
+    //Serial.println(analogRead(4));
     lcd.begin(16, 2);
-    lcd.print("1",but1,"2",but2,"3",but3,"4",but4);
-    delay(1000/rerate)
+    lcd.print(String(("1",but1,"2",but2,"3",but3,"4",but4)));
+    delay(1000/rerate);
   }
 }
