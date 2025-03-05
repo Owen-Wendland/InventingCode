b4cc04d53cbee4f0acc348aa08a66a9356365d1f
Owen Wendland <90295568+Owen-Wendland@users.noreply.github.com>
2025-03-05 11:26:45 -0800

    Added data storage to test

--- InventingCodeV1.ino
+++ InventingCodeV1.ino
@@ -1,5 +1,40 @@
 #include <LiquidCrystal.h>
+#include <EEPROM.h>
 
+void erase(void)
+{
+  for (int i = 0 ; i < EEPROM.length() ; i++)
+	EEPROM.write(i, 0);
+}
+
+void printMessage(byte* first, size_t len)
+{
+  for (int i = 0; i < len; i++)
+  {
+	Serial.print((char)first[i]);
+  }
+}
+
+void writeMsg(byte* first, size_t len)
+{
+  for(int i = 0; i < len; i++)
+  {
+	EEPROM.write(i, first[i]);
+  }
+}
+
+void readMsg(size_t len)
+{
+  byte res;
+ 
+  Serial.print("Message: ");
+  for(int i = 0; i < len; i++)
+  {
+	res = EEPROM.read(i);
+	Serial.print((char)res);
+  }
+  Serial.println("");
+}
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -28,6 +63,7 @@ int vals[4] = {0,0,0,0};
 int oldVals[4] = {0,0,0,0};
 
 void setup() {
+  char* data = "";
   lcd.begin(16, 2);
   Serial.begin(9600);
   for(int pin = 2; pin <= 5; pin++){
@@ -43,30 +79,36 @@ void loop() {
   vals[3] = !digitalRead(5);
 
   if (vals != aa){
+    erase();
     if (oldVals[0] != vals[0]){
+      data = "1";
       oldVals[0] = ab[0];
       oldVals[1] = ab[1];
       oldVals[2] = ab[2];
       oldVals[3] = ab[3];
     }
     if (oldVals[1] != vals[1]){
+      data = "2";
       oldVals[0] = ac[0];
       oldVals[1] = ac[1];
       oldVals[2] = ac[2];
       oldVals[3] = ac[3];
     }
     if (oldVals[2] != vals[2]){
+      data = "3";
       oldVals[0] = ad[0];
       oldVals[1] = ad[1];
       oldVals[2] = ad[2];
       oldVals[3] = ad[3];
     }
     if (oldVals[3] != vals[3]){
+      data = "4";
       oldVals[0] = ae[0];
       oldVals[1] = ae[1];
       oldVals[2] = ae[2];
       oldVals[3] = ae[3];
     }
+    writeMsg(data, 2);
   }
 
   //*oldVals = changed(vals, oldVals);
@@ -92,32 +134,7 @@ void loop() {
     String tempere = "temp:"+String(translation);
     lcd.print(tempere);
   }
+  readMsg(12);
   delay(1000/rerate);
   time += 1;
 }
-/*
-int *changed(int currentList[], int oldList[]){
-  int output[4] = {0,0,0,0};
-  Serial.println(oldList[1]);
-  if(currentList == oldList){
-    return oldList;
-  }
-  else{
-    if (currentList[0] != oldList[0]){
-      int a[] = {1,0,0,0};
-      return a;
-    }
-    else if (currentList[1] != oldList[1]){
-      int a[] = {0,1,0,0};
-      return a;
-    }
-    else if (currentList[2] != oldList[2]){
-      int a[] = {0,0,1,0};
-      return a;
-    }
-    else{
-      int a[] = {0,0,0,1};
-      return a;
-    }
-  }
-}*/
